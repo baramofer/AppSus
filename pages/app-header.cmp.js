22236e16241a9a206bf148cf37e9f31b628e0e61
ofer baram <my.phone.il@gmail.com>
2019-11-06 13:17:45 +0200

    CSS

--- pages/app-header.cmp.js
+++ pages/app-header.cmp.js
@@ -2,11 +2,17 @@
 
 export default {
     template: `
-    <nav>
-                <router-link to="/">Home</router-link> |
-                <router-link to="/mail">Mail</router-link> |
-                <router-link to="/keep">Keep</router-link> |
-                <!-- <router-link to="/books">Books</router-link> | -->
-    </nav>
+    <section class="header-container">
+        <div class="logo">
+            <i class="fas fa-hat-cowboy-side"></i>
+            <span style="color:grey">AppSus</span>
+        </div>
+        <nav class="nav-container">
+                   <router-link to="/">Home</router-link>|
+                    <router-link to="/mail">Mail</router-link>|
+                    <router-link to="/keep">Keep</router-link>
+                    <!-- <router-link to="/books">Books</router-link> | -->
+        </nav>
+    </section>
     `
 }
