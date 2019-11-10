9560fa1e0a17ed64eae6a4505bb5650b34a8c5c6
Snir Shahar <shahar.snir91@gmail.com>
2019-11-10 23:46:12 +0200

    homepage-image responsive

--- js/pages/home.cmp.js
+++ js/pages/home.cmp.js
@@ -3,7 +3,7 @@
 export default {
     template: `
         <section class="home-page">
-            <img src="https://images.unsplash.com/photo-1507162728832-5b8fdb5f99fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"/>
+            <img src="https://images.unsplash.com/photo-1546776310-eef45dd6d63c?ixlib=rb-1.2.1&auto=format&fit=crop&w=439&q=80"/>
             </section>
     `,
 }
