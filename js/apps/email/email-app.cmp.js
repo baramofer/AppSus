ec89f4e54a36b12b69f355dc46ce63372f589068
ofer baram <my.phone.il@gmail.com>
2019-11-10 13:00:37 +0200

    1300 ofer changes

--- js/apps/email/email-app.cmp.js
+++ js/apps/email/email-app.cmp.js
@@ -9,8 +9,7 @@ export default {
         <section class="email-main-container">
             <email-list></email-list>
                 <div class="flex column side center">
-            <side-nav>
-            </side-nav>
+            <side-nav></side-nav>
             <div class="emails-container">
             </div>
         </div>
