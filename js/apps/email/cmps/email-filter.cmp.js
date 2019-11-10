efed29c3f930016cb002406e1930f2fac3d2cb74
Snir Shahar <shahar.snir91@gmail.com>
2019-11-10 10:00:41 +0200

    sunday css changes

--- js/apps/email/cmps/email-filter.cmp.js
+++ js/apps/email/cmps/email-filter.cmp.js
@@ -3,16 +3,16 @@ import countersCmp from './counter.cmp.js';
 export default {
     props: [],
   template: `
-    <section class="mail-filter flex ">
-        <div class="flex align-center row-reverse">
+    <section class="mail-filter flex align-center row-reverse">
+        <!-- <div class="flex align-center row-reverse"> -->
             <input type="text" v-model="filterBy.txt" @input="emitFilter" placeholder="🔍 Search mail" />
-   <select v-model="filterBy.isRead" @change="emitFilter($event)">
-         <option value="All">All</option>
-         <option value="Read">Read</option>
-        <option value="Unread">Unread</option>
-  </select>
-       </div>
-        </section>
+            <select v-model="filterBy.isRead" @change="emitFilter($event)">
+                  <option value="All">All</option>
+                  <option value="Read">Read</option>
+                  <option value="Unread">Unread</option>
+            </select>
+        <!-- </div> -->
+    </section>
 `,
   data() {
     return {
